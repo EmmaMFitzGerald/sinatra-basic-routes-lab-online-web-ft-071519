2752d5a8a98bbf1efecfc75de0f050f159444abd
Emma FitzGerald <emmamariafitzgerald@gmail.com>
2019-08-22 19:56:29 +0000

    Automatically backed up by Learn

--- app.rb
+++ app.rb
@@ -2,4 +2,15 @@ require_relative 'config/environment'
 
 class App < Sinatra::Base
   
+  get '/name' do 
+    "My name is _"
+  end
+  
+  get '/hometown' do
+    "Hy hometown is _"
+  end
+  
+  get '/favourite-song' do
+    "My favourite song is _"
+  end
 end
